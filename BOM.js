424c737657d15cb12a5c282b5e70c9f3aeea3e29
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-05 22:19:14 +0600

    alert promt confrim

--- BOM.js
+++ BOM.js
@@ -11,6 +11,19 @@ const openAlert = () =>{
      }
 };
 const openPromt = () =>{
-    confirm('Are You Coming on Home?')
+    const diccisuon =  confirm('Are You Coming on Home?')
+    console.log(diccisuon)
+    if(diccisuon === true){
+        alert('Come with a condom');
+    }else{
+        console.log("DGM")
+    }
     // prompt()
+};
+const getInfo = () =>{
+    const style = prompt('Tell Us your styles?');
+    console.log(style);
+    if(!!style){
+        console.log('I am ready for play in', style)
+    }
 }
