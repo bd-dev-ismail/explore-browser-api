77c92c60bfee220c101cca15b1dc6d4353bc3451
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-05 22:07:34 +0600

    first commit

---
+++ BOM.js
@@ -0,0 +1,15 @@
+// setTimeout(() => {
+//     alert('Hello')
+// }, 3000);
+
+const openAlert = () =>{
+     const num = Math.random()*10;
+     console.log(num)
+     if(num < 5){
+
+         alert('You ar click onk alert button');
+     }
+};
+const openPromt = () =>{
+    prompt()
+}
