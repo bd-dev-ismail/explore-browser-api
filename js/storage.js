9b0b82e905102c41bfd841bac8c69ac95cb37804
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-06 14:34:50 +0600

    explore localstorage

---
+++ js/storage.js
@@ -0,0 +1,5 @@
+document.getElementById("btn-add-name").addEventListener('click', function(){
+    const inputField = document.getElementById("name-field");
+    const inputFieldString = inputField.value;
+    console.log(inputFieldString)
+});
